2ca159a214b44e6ea7ec5b2a46976b63b06ffe9c
Andy Wilson <andy.wilson1234@gmail.com>
2025-05-28 09:26:40 +0100

    fix: remove comment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,6 @@
 const Footer = () => {
   return (
     <footer className="bg-purple-900/90 backdrop-blur-sm p-4 text-white">
-      {/* <p>&copy; {new Date().getFullYear()} Forge Interactive Sheets</p> */}
       <p>This site uses only official Public Game Content from the Darrington Press System Reference Document (SRD), shared under the Community Gaming License. The descriptive text comes straight from the SRD, but all the images here are original creations inspired by those descriptions. We don’t use any copyrighted art, logos, or story content from Critical Role or its related companies. All trademarks and rights belong to Critical Role, LLC and its partners. Just so you know, this project isn’t officially connected to or endorsed by Critical Role or Darrington Press.</p>
     </footer>
   );
