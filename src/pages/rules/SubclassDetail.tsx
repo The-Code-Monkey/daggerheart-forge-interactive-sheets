a72c41ac755126ceedac2f5e13c675a7963eb2ae
Andy Wilson <andy.wilson1234@gmail.com>
2025-06-06 14:16:52 +0100

    subclasses now visable

--- src/pages/rules/SubclassDetail.tsx
+++ src/pages/rules/SubclassDetail.tsx
@@ -1,10 +1,5 @@
 import { useParams, Link, useNavigate } from "react-router-dom";
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
 import { JSX, useEffect, useState } from "react";
@@ -79,7 +74,8 @@ const SubclassDetail = (): JSX.Element => {
           <div className="flex items-center gap-4 mb-4">
             <div>
               <h1 className="text-4xl font-bold text-white">
-                {subclassData.name}
+                {subclassData.name}{" "}
+                {subclassData.isHomebrew ? "(Homebrew)" : ""}
               </h1>
               <p className="text-xl text-purple-200">
                 {subclassData.description}
@@ -89,25 +85,83 @@ const SubclassDetail = (): JSX.Element => {
         </div>
 
         {/* Class Stats */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-8">
-          {/* <Card className="bg-linear-to-br from-slate-800/80 to-slate-900/80 border-purple-500/30 backdrop-blur-xs">
+        <div className="grid grid-cols-1 gap-6 mb-8">
+          <Card className="bg-linear-to-br from-slate-800/80 to-slate-900/80 border-purple-500/30 backdrop-blur-xs">
             <CardHeader>
-              <CardTitle className="text-white">Available Domains</CardTitle>
+              <CardTitle className="text-white">Foundation Features</CardTitle>
             </CardHeader>
             <CardContent>
-              <div className="flex flex-wrap gap-1">
-                {classData.domains.map((domain) => (
-                  <Badge
-                    key={domain.id}
-                    variant="outline"
-                    className="border-yellow-500/50 text-yellow-300 text-xs"
-                  >
-                    {domain.name}
-                  </Badge>
-                ))}
+              <div className="flex flex-wrap text-white divide-y">
+                {subclassData.features.foundation?.map((feature, index) => {
+                  return (
+                    <div key={index} className="py-4 w-full">
+                      <span className="font-bold">{feature.name}</span>
+                      <p className="mt-4">{feature.description}</p>
+                      {feature.list && (
+                        <ul className="list-disc ml-4 mt-4">
+                          {feature.list.map((item, i) => (
+                            <li key={i}>{item}</li>
+                          ))}
+                        </ul>
+                      )}
+                    </div>
+                  );
+                })}
               </div>
             </CardContent>
-          </Card> */}
+          </Card>
+
+          <Card className="bg-linear-to-br from-slate-800/80 to-slate-900/80 border-purple-500/30 backdrop-blur-xs">
+            <CardHeader>
+              <CardTitle className="text-white">
+                Specialization Features
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-wrap text-white divide-y">
+                {subclassData.features.specialization?.map((feature, index) => {
+                  return (
+                    <div key={index} className="py-4 w-full">
+                      <span className="font-bold">{feature.name}</span>
+                      <p className="mt-4">{feature.description}</p>
+                      {feature.list && (
+                        <ul className="list-disc ml-4 mt-4">
+                          {feature.list.map((item, i) => (
+                            <li key={i}>{item}</li>
+                          ))}
+                        </ul>
+                      )}
+                    </div>
+                  );
+                })}
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card className="bg-linear-to-br from-slate-800/80 to-slate-900/80 border-purple-500/30 backdrop-blur-xs">
+            <CardHeader>
+              <CardTitle className="text-white">Mastery Features</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-wrap text-white divide-y">
+                {subclassData.features.mastery?.map((feature, index) => {
+                  return (
+                    <div key={index} className="py-4 w-full">
+                      <span className="font-bold">{feature.name}</span>
+                      <p className="mt-4">{feature.description}</p>
+                      {feature.list && (
+                        <ul className="list-disc ml-4 mt-4">
+                          {feature.list.map((item, i) => (
+                            <li key={i}>{item}</li>
+                          ))}
+                        </ul>
+                      )}
+                    </div>
+                  );
+                })}
+              </div>
+            </CardContent>
+          </Card>
 
           {/* <Card className="bg-linear-to-br from-slate-800/80 to-slate-900/80 border-purple-500/30 backdrop-blur-xs">
             <CardHeader>
@@ -118,40 +172,6 @@ const SubclassDetail = (): JSX.Element => {
             </CardContent>
           </Card> */}
         </div>
-
-        {/* Class Features */}
-        <Card className="bg-linear-to-br from-slate-800/80 to-slate-900/80 border-purple-500/30 backdrop-blur-xs col-span-2">
-          <CardHeader>
-            <CardTitle className="text-white">Class Features</CardTitle>
-            <CardDescription className="text-purple-200">
-              {/* Special abilities that define the {classData.name} class */}
-            </CardDescription>
-          </CardHeader>
-          {/* <CardContent>
-            <div className="space-y-4">
-              {classData.features.map((feature, index) => (
-                <div key={`feature-${String(index)}`}>
-                  <h3 className="text-lg font-semibold text-white mb-2">
-                    {feature.name}
-                  </h3>
-                  <p className="text-purple-200">{feature.description}</p>
-                  {feature.list && (
-                    <ul className="list-disc list-inside">
-                      {feature.list.map((item, itemIndex) => (
-                        <li
-                          key={`feature-list-${String(itemIndex)}`}
-                          className="text-purple-200"
-                        >
-                          {item}
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </div>
-              ))}
-            </div>
-          </CardContent> */}
-        </Card>
       </div>
     </div>
   );
